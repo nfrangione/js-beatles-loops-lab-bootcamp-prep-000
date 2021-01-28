799d6e74b70d94bb6ea988b4e7f013d2469f235e
Nicholas Frangione <nfrangione8@gmail.com>
2021-01-28 22:25:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // add solution here
-function theBeatlesPlay() {
-  
+
+function theBeatlesPlay(musicians, instruments) {
+  var musicianInstrument = new Array();
+  for (i = 0; i < musicians.length; i++) {
+    musicianInstrument[i] = `${musicians[i]} plays ${instrument[i]}`
+  }
+  return musicianInstrument;
 }
